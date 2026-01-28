bbc948ecf8cfc9631587b75e6f539c2f9fc5f5a1
fujita <fujita0000hisashi@gmail.com>
2026-01-28 13:58:39 +0900

    feat: ユーザー一覧のデータ表示

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Routing\Attributes\Middleware;
+use App\Models\User;
 
 class UserController extends Controller
 {
-  public function showUsers ($status = 'active') {
-    // return User::whereStatus($status)->get();
-    return view('admin.user');
+  // public function showUsers ($status = 'active') {
+  //   $user = User::whereStatus($status)->get();
+  //   return view('admin.user', compact('user'));
+  // }
+  public function showUsers () {
+    $users = User::All();
+    return view('admin.user', compact('users'));
   }
 }
--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -11,5 +11,34 @@
             <li><a href="#">アカウント一覧</a></li>
         </ul>
     </div>
+    <div class="adminTebleArea">
+        <table class="teble">
+            <thead class="tebleThead">
+                <tr>
+                    <th scope="col">編集</th>
+                    <th scope="col">名前</th>
+                    <th scope="col">メールアドレス</th>
+                    <th scope="col">電話番号</th>
+                    <th scope="col">都道府県</th>
+                    <th scope="col">市町村</th>
+                    <th scope="col">番地・アパート名</th>
+                </tr>
+            </thead>
+            <tbody class="tebleTbody">
+                @foreach($users as $user)
+                <tr>
+                    
+                    <td><a href="#" class="tableCreateLink">編集</a></td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->phone }}</td>
+                    <td>{{ $user->prefecture }}</td>
+                    <td>{{ $user->city }}</td>
+                    <td>{{ $user->address }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 @endsection
