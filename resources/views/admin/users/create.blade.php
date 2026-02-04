d4597d1a03f1def3bf391e20b85d1f060511fecb
fujita <fujita0000hisashi@gmail.com>
2026-02-04 14:43:08 +0900

    fix: 新規登録ボタンの修正

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -6,9 +6,6 @@
 <div class="contentsArea">
   <div class="contentsArea__header">
     <h2 class="contentsArea__header__title">{{$title}}</h2>
-    <ul class="contentsArea__header__btnArea">
-      <li><a href="#">新規登録</a></li>
-    </ul>
   </div>
   <div class="contentsArea__content">
     <form action="{{ route('admin.users.confirm') }}" method="POST">
