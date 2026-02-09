f35d89c834fd78d3541f33b4730acf7ddad4e886
fujita <fujita0000hisashi@gmail.com>
2026-02-09 11:40:44 +0900

    お問い合わせ編集ページにstatus、memoの入力欄を追加

--- resources/views/admin/contact/edit.blade.php
+++ resources/views/admin/contact/edit.blade.php
@@ -4,31 +4,64 @@
 
 {{ Breadcrumbs::render('admin.contact.edit') }}
 <div class="contentsArea">
-    <div class="contentsArea__header">
-        <h2 class="contentsArea__header__title">{{$title}}</h2>
-    </div>
-    <div class="contentsArea__content">
-        <div class="adminTebleArea">
-            <table class="teble">
-                <thead class="tebleThead">
-                    <tr>
-                        <th scope="col">ステータス</th>
-                        <th scope="col">会社名</th>
-                        <th scope="col">氏名</th>
-                        <th scope="col">電話番号</th>
-                    </tr>
-                </thead>
-                <tbody class="tebleTbody">
-                    <tr>
-                        <td>{{ $contact->name }}</td>
-                        <td>{{ $contact->status }}</td>
-                        <td>{{ $contact->company }}</td>
-                        <td>{{ $contact->phone }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
+  <div class="contentsArea__header">
+    <h2 class="contentsArea__header__title">{{$title}}</h2>
+  </div>
+  <div class="contentsArea__content">
+    <section class="admin-contact">
+        <form action="#">
+          <div class="dataContentItem">
+            <h3>ステータス</h3>
+            <select id="status" name="status" class="select">
+              <option value="">未対応</option>
+              <option value="対応中">対応中</option>
+              <option value="完了">対応済</option>
+            </select>
+            @error('status')
+            <span class="errorMessage">{{ $message }}</span>
+            @enderror
+          </div>
+          <div class="dataContentItem">
+            <h3>お問い合わせ内容</h3>
+            <p class="dataContentItemP">{{ $contact['contact'] }}</p>
+          </div>
+          <div class="dataContentItem">
+            <h3>備考欄</h3>
+            <textarea id="memo" name="memo" class="textArea"></textarea>
+            @error('memo')
+            <span class="errorMessage">{{ $message }}</span>
+            @enderror
+          </div>
+          <div>
+            <h3>お問い合わせ情報</h3>
+            <div class="dataContentItem">
+              <p class="dataContentItemP">会社名:{{ $contact['company'] }}</p>
+            </div>
+            <div class="dataContentItem">
+              <p class="dataContentItemP">氏名:{{ $contact['name'] }}</p>
+            </div>
+            <div class="dataContentItem">
+              <p class="dataContentItemP">電話番号:{{ $contact['phone'] }}</p>
+            </div>
+            <div class="dataContentItem">
+              <p class="dataContentItemP">メールアドレス:{{ $contact['mail'] }}</p>
+            </div>
+            <div class="dataContentItem">
+              <p class="dataContentItemP">生年月日:{{ $contact['birthday'] }}</p>
+            </div>
+            <div class="dataContentItem">
+              <p class="dataContentItemP">性別:{{ $contact['sex'] }}</p>
+            </div>
+            <div class="dataContentItem">
+              <p class="dataContentItemP">職業:{{ $contact['job'] }}</p>
+            </div>
+          </div>
+          @csrf
+          <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+          <button type="submit" class="submitButton">登録する</button>
+          <!-- <button type="button" class="submitButton" onclick="history.back()">戻る</button> -->
+        </form>
+    </section>
+  </div>
 </div>
-
 @endsection
