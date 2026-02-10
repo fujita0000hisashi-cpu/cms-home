bda39008a6f4f7b760d406bd08e204657846e5f2
fujita0000hisashi-cpu <fujita0000hisashi@gmail.com>
2026-02-10 14:32:16 +0900

    fix: メールアドレスのバリデーションを修正 (#8)

--- resources/lang/ja/validation.php
+++ resources/lang/ja/validation.php
@@ -1,4 +1,21 @@
 <?php
   return [
-    'required' => ':attribute は必須項目です。'
+    'required' => ':attribute は必須項目です。',
+    'custom' => [
+      'mail' => [
+        'unique' => 'このメールアドレスは既に登録されています。'
+      ]
+    ],
+    'attributes' => [
+      'name' => '名前',
+      'name_kana' => 'フリガナ',
+      'mail' => 'メールアドレス',
+      'password' => 'パスワード',
+      'phone' => '電話番号',
+      'postcode' => "郵便番号",
+      'prefecture' => '都道府県',
+      'city' => '市町村',
+      'address_line1' => '番地・アパート名',
+      'memo' => '備考欄'
+    ]
   ];
