3f0006a1c565288861495df5879a097f7dae0d88
fujita0000hisashi-cpu <fujita0000hisashi@gmail.com>
2026-02-10 12:46:45 +0900

    Feature/aside menu (#7)
    * asideメニューにお問い合わせ一覧を追加
    
    * chor: asideメニューのレイアウト調整

--- resources/views/parts/aside.blade.php
+++ resources/views/parts/aside.blade.php
@@ -1,3 +1,7 @@
+@php
+    $active = 'bg-gray-500 font-semibold';
+    $normal = '';
+@endphp
 <aside class="adminAside">
     <!-- ハンバーガーメニュー -->
     <div class="header__hamburger">
@@ -12,11 +16,14 @@
     <!-- ナビ -->
     <nav class="adminAside__nav" id="js-nav">
         <ul>
-            <li>
+            <li class="{{ request()->routeIs('admin.index') ? $active : $normal }}">
                 <a href="/admin"><i class="fa-solid fa-house-chimney"></i>HOME</a>
             </li>
-            <li>
+            <li class="{{ request()->routeIs('admin.users*') ? $active : $normal }}">
                 <a href="/users"><i class="fa-solid fa-envelopes-bulk"></i>アカウント一覧</a>
+            </li>
+            <li class="{{ request()->routeIs('admin.contact.*') ? $active : $normal }}">
+                <a href="/admin/contact"><i class="fa-solid fa-envelopes-bulk"></i>お問い合わせ一覧</a>
             </li>
         </ul>
     </nav>
