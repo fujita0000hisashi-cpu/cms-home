29b6b24707d07c6b3450db204aaae1f1dba4e934
fujita <fujita0000hisashi@gmail.com>
2026-02-12 11:54:21 +0900

    refactor: 共通フォームを _form.blade.php に分離

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,21 @@ Route::post('/contact/send', [ContactController::class, 'send'])->name('contact.
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login.show');
 Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
+
+// admin
 Route::get('/admin', [AdminController::class, 'showTop'])->name('admin.index');
 Route::get('/admin/contact', [AdminContactController::class, 'showContacts'])->name('admin.contact.index');
 Route::get('/admin/contact/{id}/edit', [AdminContactController::class, 'edit'])->name('admin.contact.edit');
 Route::put('/admin/{contact}', [AdminContactController::class, 'update'])->name('admin.contact.update');
 
-// Route::view('/users', 'admin.users')->name('admin.users');
+// ユーザー登録
+// 新規
 Route::get('/users/create', [UserController::class, 'createUser'])->name('admin.users.create');
 Route::post('/users/confirm', [UserController::class, 'confirm'])->name('admin.users.confirm');
 Route::post('/users/send', [UserController::class, 'send'])->name('admin.users.send');
+
+// 編集
+// Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('admin.users.edit');
+
+// ユーザー一覧
 Route::get('/users/{status?}', [UserController::class, 'showUsers'])->name('admin.users');
